6fce0cfa8b207f1546c2c0b002601556a3171221
Bruno Cardoso <bruno@liveonsolutions.com>
2021-12-22 22:37:59 -0300

    refactor: expose clases

--- src/commands/auth.ts
+++ src/commands/auth.ts
@@ -1,19 +1,76 @@
 import { GluegunToolbox } from 'gluegun'
 
-import CreateAuthService from '../extensions/services/CreateAuthService'
+import { makeGetPromptCommunication } from '@shared/providers'
+import { makeGetCreateAuthentication } from '../modules/authentication'
 
 module.exports = {
   name: 'make:auth',
   description: 'A complete authentication module using JWT',
   run: async (toolbox: GluegunToolbox) => {
-    const createAuth = new CreateAuthService(toolbox)
+    const actions = [
+      {
+        template: 'js/src/app/validators/userValidator.js.ejs',
+        target: 'src/app/validators/UserValidator.js'
+      },
+      {
+        template: 'src/config/auth.js.ejs',
+        target: 'src/config/auth.js'
+      },
+      {
+        template: 'js/src/app/models/userModel.js.ejs',
+        target: 'src/app/models/UserModel.js'
+      },
+      {
+        template: 'js/src/app/controllers/userController.js.ejs',
+        target: 'src/app/controllers/UserController.js'
+      },
+      {
+        template: 'js/src/app/middlewares/authentication.js.ejs',
+        target: 'src/app/middlewares/AuthenticationMiddleware.js'
+      },
+      {
+        template: 'js/src/app/controllers/sessionController.js.ejs',
+        target: 'src/app/controllers/SessionController.js'
+      },
+      {
+        template: 'js/src/app/routes/session.router.js.ejs',
+        target: 'src/app/routes/session.router.js'
+      },
+      {
+        template: 'js/src/app/routes/user.router.js.ejs',
+        target: 'src/app/routes/user.router.js'
+      }
+    ]
 
-    await createAuth.execute()
-
-    await toolbox.system.spawn(`npx eslint src/ --fix`, {
-      shell: true,
-      stdio: 'inherit',
-      stderr: 'inherit'
+    const authentication = await makeGetCreateAuthentication().execute({
+      actions
     })
+
+    const communicate = makeGetPromptCommunication()
+
+    if (!authentication.success) {
+      return communicate.execute({
+        type: 'error',
+        message: authentication.data.message
+      })
+    }
+
+    communicate.execute({
+      type: 'warning',
+      message: 'Start project linting...'
+    })
+
+    toolbox.system
+      .spawn(`npx eslint src/ --fix`, {
+        shell: true,
+        stdio: 'inherit',
+        stderr: 'inherit'
+      })
+      .finally(() =>
+        communicate.execute({
+          type: 'success',
+          message: 'Your authentication package is created'
+        })
+      )
   }
 }
